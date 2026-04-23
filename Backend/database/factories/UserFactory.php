775c834a7462120647a410b6c904130a16397296
ivuuw <kertesz.ivett@blathy.info>
2026-04-23 11:32:32 +0200

    Authentikáció hozzáadva

---
+++ Backend/database/factories/UserFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'password' => bcrypt('password'),
+            'role' => 'user'
+        ];
+    }
+}
---
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin'
+        ]);
+
+        User::factory(10)->create();
+    }
+}
