0a6f2818d072de70a79b0b39cee955f09c40d7f0
ivuuw <kertesz.ivett@blathy.info>
2026-04-23 11:54:05 +0200

    Authentikáció hozzáadva

--- Backend/database/seeders/DatabaseSeeder.php
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -4,18 +4,43 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\NailArtist;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        User::create([
+        // ADMIN
+        $admin = User::create([
             'name' => 'Admin',
             'email' => 'admin@test.com',
             'password' => bcrypt('password'),
             'role' => 'admin'
         ]);
 
-        User::factory(10)->create();
+        // ARTIST USER
+        $artistUser = User::create([
+            'name' => 'Artist User',
+            'email' => 'artist@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'artist'
+        ]);
+
+        // USER
+        $user = User::create([
+            'name' => 'Normal User',
+            'email' => 'user@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'user'
+        ]);
+
+        // ARTIST PROFIL
+        NailArtist::create([
+            'user_id' => $artistUser->id,
+            'name' => 'Pro Körmös',
+            'city' => 'Budapest',
+            'description' => 'Gél lakkozás specialista',
+            'approved' => true
+        ]);
     }
 }
